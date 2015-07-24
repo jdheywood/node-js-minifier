aedcc5eca1d2d6471cd265e9f640d12060b6a7e6
James Heywood <jdheywood@yahoo.co.uk>
2015-07-24 21:52:55 +0100

    logic fixes and notes update

--- minifier.js
+++ minifier.js
@@ -19,34 +19,35 @@ function readFile(filename) {
 
 function isAlphanumeric(input) {
 	var expr = /^([0-9]|[a-z])+([0-9a-z]+)$/i
-	return !!input.match(expr);
+	return expr.test(input);
 }
 
 function isEndspace(input) {
-	return (input === '\n' || input === '\r' || input === '\f')
+	return (input == '\n' || input == '\r' || input == '\f')
 }
 
 function isWhitespace(input) {
-	return (input === ' ' || input === "\t" || input === "\n" || input === "\r" || input === "\f");
+	var expr = /\s/
+	return expr.test(input);
 }
 
 /* New line characters before or after these characters can be removed.
    Not + - / in this list because they require special care. */
 function isInfix(input) {
 	var expr = /[,;:=&%*<>\?\|\n]/;
-	return !!input.match(expr);	
+	return expr.test(input);
 }
 
 // New line characters after these characters can be removed.
 function isPrefix(input) {
 	var expr = /[\{\(\[!]/; 
-	return (!!input.match(expr) || isInfix(input));
+	return (expr.test(input) || isInfix(input));
 }
 
 // New line characters before these characters can removed.
 function isPostfix(input) {
 	var expr = /[\}\)\]]/; 
-	return (!!input.match(expr) || isInfix(input));
+	return (expr.test(input) || isInfix(input));
 }
 
 function getChar() {
@@ -186,116 +187,115 @@ function minify(filename, stripDebug) {
 
 	while (defined(myCharA)) {
 
-		if (defined(myCharA)) {
-
-			if (isWhitespace(myCharA)) {
-				throw 'minifier bug: minify loop starting with whitespace, barfed!';
-			}
-
-			// Each branch handles trailing whitespace and ensures a is on non-whitespace or is undefined when branch finishes
-			if (myCharA == '/') { // a division, comment, or regexp literal
-		      	if (defined(myCharB) && myCharB == '/') { // slash-slash comment
-		        	ccFlag = defined(myCharC) && myCharC == '@'; // tests in IE7 show no space allowed between slashes and at symbol
-		        	do {
-		          		ccFlag ? action2() : action3();
-		        	} while (defined(myCharA) && !isEndspace(myCharA));
-		        	if (defined(myCharA)) { // a is a new line
-		          		if (ccFlag) {
-		            		action1(); // cannot use preserveEndspace() here because it might not print the new line
-		            		skipWhitespace();
-		          		}
-		          		else if (defined(myLast) && !isEndspace(myLast) && !isPrefix(myLast)) {
-		            		preserveEndspace();
-		          		}
-		          		else {
-		            		skipWhitespace();
-		          		}
-		        	}
-		      	}
-				else if (defined(myCharB) && myCharB == '*') { // slash-star comment
-		    		ccFlag = defined(myCharC) && myCharC == '@'; // test in IE7 shows no space allowed between star and at symbol
-		    		do {
-		      			ccFlag ? action2() : action3();
-		    		} while (defined(myCharB) && (myCharA != '*' && myCharB != '/'));
-		    		if (defined(myCharB)) { // a is asterisk and b is foreslash
-		      			if (ccFlag) {
-		        			action2(); // the *
-		        			action2(); // the /
-		        			// inside the conditional comment there may be a missing terminal semi-colon
-		        			preserveEndspace();
-		      			}
-		      			else { // the comment is being removed
-		        			action3(); // the *
-		        			myCharA = ' ';  // the /
-		        			collapseWhitespace();
-		        			if (defined(myLast) && defined(myCharB) &&
-		            			((isAlphanumeric(myLast) && (isAlphanumeric(myCharB) || myCharB == '.')) ||
-		             			(myLast == '+' && myCharB == '+') || (myLast == '-' && myCharB == '-'))) { // for a situation like 5-/**/-2 or a/**/a
-		          				// When entering this block a is whitespace.
-		          				// The comment represented whitespace that cannot be removed. Therefore replace the now gone comment with a whitespace.
-		          				action1();
-		        			}
-		        			else if (defined(myLast) && !isPrefix(myLast)) {
-		          				preserveEndspace();
-		        			}
-		        			else {
-		          				skipWhitespace();
-		        			}
-		      			}
-		    		}
-		    		else {
-		      			throw 'unterminated comment, stopped';
-		    		}
-		  		}
-		  
-		  		else if ( defined(myLastNws) && (myLastNws == ')' || myLastNws == ']' || myLastNws == '.' || isAlphanumeric(myLastNws)) ) { // division
-		    		action1();
-		    		collapseWhitespace();
-		    		// don't want a division to become a slash-slash comment with following conditional comment
-		    		onWhitespaceConditionalComment() ? action1() : preserveEndspace();
-		  		}
-		  		else { // regexp literal
-		    		putLiteral();
-		    		collapseWhitespace();
-		    		// don't want closing delimiter to become a slash-slash comment with following conditional comment
-		    		onWhitespaceConditionalComment() ? action1() : preserveEndspace();
-		  		}
-			}
-			else if (myCharA == '\'' || myCharA == '"' ) { // string literal
-		  		putLiteral();
-		  		preserveEndspace();
-			}
-			else if (myCharA == '+' || myCharA == '-') { // careful with + + and - -
-		  		action1();
-		  		collapseWhitespace();
-		  		if (defined(myCharA) && isWhitespace(myCharA)) {
-		    		(defined(myCharB) && myCharB == myLast) ? action1() : preserveEndspace();
-		  		}
-			}
-			else if (isAlphanumeric(myCharA)) { // keyword, identifiers, numbers
-		  		action1();
-		  		collapseWhitespace();
-		  		if (defined(myCharA) && isWhitespace(myCharA)) {
-		    		// if b is '.' could be (12 .toString()) which is property invocation. If space removed becomes decimal point and error.
-		    		(defined(myCharB) && (isAlphanumeric(myCharB) || myCharB == '.')) ? action1() : preserveEndspace();
-		  		}
-			}
-			else if (myCharA == ']' || myCharA == '}' || myCharA == ')') { // no need to be followed by space but maybe needs following new line
-		  		action1();
-		  		preserveEndspace();
-			}
-			else if (stripDebug && myCharA == ';' &&
-		       		defined(myCharB) && myCharB == ';' &&
-		       		defined(myCharC) && myCharC == ';') {
-		  		action3(); // delete one of the semi-colons
-		  		myCharA = '/'; // replace the other two semi-colons
-		  		myCharB = '/'; // so the remainder of line is removed
-			}
-			else { // anything else just prints and trailing whitespace discarded
-		  		action1();
-		  		skipWhitespace();
-			}
+		if (isWhitespace(myCharA)) {
+			throw 'minifier bug: minify loop starting with whitespace, barfed!';
 		}
+
+		// Each branch handles trailing whitespace and ensures a is on non-whitespace or is undefined when branch finishes
+		if (myCharA == '/') { // a division, comment, or regexp literal
+	      	if (defined(myCharB) && myCharB == '/') { // slash-slash comment
+	        	ccFlag = defined(myCharC) && myCharC == '@'; // tests in IE7 show no space allowed between slashes and at symbol
+	        	while (defined(myCharA) && !isEndspace(myCharA)) {
+	        		ccFlag ? action2() : action3();
+	        	}
+	        	if (defined(myCharA)) { // a is a new line
+	          		if (ccFlag) {
+	            		action1(); // cannot use preserveEndspace() here because it might not print the new line
+	            		skipWhitespace();
+	          		}
+	          		else if (defined(myLast) && !isEndspace(myLast) && !isPrefix(myLast)) {
+	            		preserveEndspace();
+	          		}
+	          		else {
+	            		skipWhitespace();
+	          		}
+	        	}
+	      	}
+			else if (defined(myCharB) && myCharB == '*') { // slash-star comment
+	    		ccFlag = (defined(myCharC) && myCharC == '@'); // test in IE7 shows no space allowed between star and at symbol
+	    		while (defined(myCharB) && !(myCharA == '*' && myCharB == '/')) {
+	      			ccFlag ? action2() : action3();
+	    		}
+	    		if (defined(myCharB)) { // a is asterisk and b is foreslash
+	      			if (ccFlag) {
+	        			action2(); // the *
+	        			action2(); // the /
+	        			// inside the conditional comment there may be a missing terminal semi-colon
+	        			preserveEndspace();
+	      			}
+	      			else { // the comment is being removed
+	        			action3(); // the *
+	        			action3();
+	        			myCharA = ' ';  // the /
+	        			collapseWhitespace();
+	        			if (defined(myLast) && defined(myCharB) &&
+	            			((isAlphanumeric(myLast) && (isAlphanumeric(myCharB) || myCharB == '.')) ||
+	             			(myLast == '+' && myCharB == '+') || (myLast == '-' && myCharB == '-'))) { // for a situation like 5-/**/-2 or a/**/a
+	          				// When entering this block a is whitespace.
+	          				// The comment represented whitespace that cannot be removed. Therefore replace the now gone comment with a whitespace.
+	          				action1();
+	        			}
+	        			else if (defined(myLast) && !isPrefix(myLast)) {
+	          				preserveEndspace();
+	        			}
+	        			else {
+	          				skipWhitespace();
+	        			}
+	      			}
+	    		}
+	    		else {
+	      			throw 'unterminated comment, stopped';
+	    		}
+	  		}
+	  
+	  		else if ( defined(myLastNws) && (myLastNws == ')' || myLastNws == ']' || myLastNws == '.' || isAlphanumeric(myLastNws)) ) { // division
+	    		action1();
+	    		collapseWhitespace();
+	    		// don't want a division to become a slash-slash comment with following conditional comment
+	    		onWhitespaceConditionalComment() ? action1() : preserveEndspace();
+	  		}
+	  		else { // regexp literal
+	    		putLiteral();
+	    		collapseWhitespace();
+	    		// don't want closing delimiter to become a slash-slash comment with following conditional comment
+	    		onWhitespaceConditionalComment() ? action1() : preserveEndspace();
+	  		}
+		}
+		else if (myCharA == '\'' || myCharA == '"' ) { // string literal
+	  		putLiteral();
+	  		preserveEndspace();
+		}
+		else if (myCharA == '+' || myCharA == '-') { // careful with + + and - -
+	  		action1();
+	  		collapseWhitespace();
+	  		if (defined(myCharA) && isWhitespace(myCharA)) {
+	    		(defined(myCharB) && myCharB == myLast) ? action1() : preserveEndspace();
+	  		}
+		}
+		else if (isAlphanumeric(myCharA)) { // keyword, identifiers, numbers
+	  		action1();
+	  		collapseWhitespace();
+	  		if (defined(myCharA) && isWhitespace(myCharA)) {
+	    		// if b is '.' could be (12 .toString()) which is property invocation. If space removed becomes decimal point and error.
+	    		(defined(myCharB) && (isAlphanumeric(myCharB) || myCharB == '.')) ? action1() : preserveEndspace();
+	  		}
+		}
+		else if (myCharA == ']' || myCharA == '}' || myCharA == ')') { // no need to be followed by space but maybe needs following new line
+	  		action1();
+	  		preserveEndspace();
+		}
+		else if (stripDebug && myCharA == ';' &&
+	       		defined(myCharB) && myCharB == ';' &&
+	       		defined(myCharC) && myCharC == ';') {
+	  		action3(); // delete one of the semi-colons
+	  		myCharA = '/'; // replace the other two semi-colons
+	  		myCharB = '/'; // so the remainder of line is removed
+		}
+		else { // anything else just prints and trailing whitespace discarded
+	  		action1();
+	  		skipWhitespace();
+		}
+		
 	}
 
 	if (defined(myLastReadChar) && myLastReadChar == '\n') {
